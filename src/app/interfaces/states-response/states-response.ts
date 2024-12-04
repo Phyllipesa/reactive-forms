048a7da9b3fbeac7c9fabc1415f068185149d0dd
Phyllipe <phyllipe.sjc@hotmail.com>
2024-12-04 11:18:11 -0300

    Criada interface IStatesResponse

---
+++ src/app/interfaces/states-response/states-response.ts
@@ -0,0 +1,6 @@
+import { IStatesResponseData } from "./states-response-data";
+import { IBaseStatesResponse } from "../base-states-response";
+
+export interface IStatesResponse extends IBaseStatesResponse {
+    data: IStatesResponseData;
+}
