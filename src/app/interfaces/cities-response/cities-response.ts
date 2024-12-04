fe7fa5f2fb3bfb67eb3739a296722bfd8098f679
Phyllipe <phyllipe.sjc@hotmail.com>
2024-12-04 12:24:01 -0300

    Criada interface ICitiesResponse

---
+++ src/app/interfaces/cities-response/cities-response.ts
@@ -0,0 +1,6 @@
+import { CitiesList } from "../../type/cities-list";
+import { IBaseCountriesResponse } from "../base-countries-response";
+
+export interface ICitiesResponse extends IBaseCountriesResponse {
+    data: CitiesList;
+}
