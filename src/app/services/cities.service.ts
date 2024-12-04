738564da7595019ccef235798694e5f430b46b07
Phyllipe <phyllipe.sjc@hotmail.com>
2024-12-04 12:24:39 -0300

    Adicionando as typagens no método getCities

--- src/app/services/cities.service.ts
+++ src/app/services/cities.service.ts
@@ -1,6 +1,8 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { map, Observable } from "rxjs";
+import { CitiesList } from "../type/cities-list";
+import { ICitiesResponse } from "../interfaces/cities-response/cities-response";
 
 @Injectable({
     providedIn: 'root'
@@ -10,10 +12,10 @@ export class CitiesService {
         private readonly _httpClient: HttpClient
     ) { }
 
-    getCities(countryName: string, stateName: string): Observable<any> {
+    getCities(countryName: string, stateName: string): Observable<CitiesList> {
         const body = { country: countryName, state: stateName }
         return this._httpClient
-            .post<any>('https://countriesnow.space/api/v0.1/countries/state/cities', body)
+            .post<ICitiesResponse>('https://countriesnow.space/api/v0.1/countries/state/cities', body)
             .pipe(
                 map(({ data }) => {
                     return data;
