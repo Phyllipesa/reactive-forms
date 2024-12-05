ff7c1eaae370e937e200927280b0ad34f58abc7b
Phyllipe <phyllipe.sjc@hotmail.com>
2024-12-05 13:54:56 -0300

    Aplicando o UserListResponse no método getUsers

--- src/app/services/users.service.ts
+++ src/app/services/users.service.ts
@@ -1,14 +1,15 @@
 import { Injectable } from "@angular/core";
 import { Observable } from "rxjs";
 import { usersList } from "../data/usersList";
+import { UserListResponse } from "../type/user-list-response";
 
 @Injectable({
     providedIn: 'root'
 })
 export class UserService {
 
-    getUsers() {
-        return new Observable((observer) => {
+    getUsers(): Observable<UserListResponse> {
+        return new Observable<UserListResponse>((observer) => {
             setTimeout(() => {
                 observer.next(usersList);
                 observer.complete();
