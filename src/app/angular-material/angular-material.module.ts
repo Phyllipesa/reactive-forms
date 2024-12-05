b46af8e12e103835b0a92c18311f788cfcedf061
Phyllipe <phyllipe.sjc@hotmail.com>
2024-12-05 15:38:24 -0300

    Adicionando MatCardModule

--- src/app/angular-material/angular-material.module.ts
+++ src/app/angular-material/angular-material.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { MatCardModule } from '@angular/material/card';
 
 @NgModule({
-    imports: [CommonModule],
-    exports: [CommonModule],
+    imports: [
+        MatCardModule,
+    ],
+    exports: [
+        MatCardModule,
+    ],
 })
-export class AngularMaterialModule {}
+export class AngularMaterialModule { }
