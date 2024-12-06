5b14f6486c01ed18b3e992fad60f096aaf8da160
Phyllipe <phyllipe.sjc@hotmail.com>
2024-12-06 16:47:22 -0300

    Configurado MaritalStatusPipe

--- src/app/pipes/marital-status.pipe.ts
+++ src/app/pipes/marital-status.pipe.ts
@@ -1,12 +1,19 @@
 import { Pipe, PipeTransform } from '@angular/core';
+import { MaritalStatusEnum } from '../enums/marital-status.enum';
 
 @Pipe({
 	name: 'maritalStatus'
 })
 export class MaritalStatusPipe implements PipeTransform {
+	transform(matiralStatus: number | undefined): string {
+		const maritalStatusMap: { [key: number]: string } = {
+			[MaritalStatusEnum.SINGLE]: 'Solteiro',
+			[MaritalStatusEnum.MARRIED]: 'Casado',
+			[MaritalStatusEnum.DIVORCED]: 'Divorciado',
+		};
 
-	transform(value: unknown, ...args: unknown[]): unknown {
-		return null;
+		return matiralStatus ? maritalStatusMap[matiralStatus] : '';
 	}
 
 }
+
