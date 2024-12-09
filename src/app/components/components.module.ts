10b61aecb0a6f63f057d68fd1c8a4ef70e2a79e5
Phyllipe <phyllipe.sjc@hotmail.com>
2024-12-09 09:22:08 -0300

    Criado ContactInformationsComponent

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -1,16 +1,18 @@
 import { NgModule } from '@angular/core';
-import { AngularMaterialModule } from '../angular-material/angular-material.module';
 import { PipesModule } from '../pipes/pipes.module';
-import { UsersListComponent } from './users-list/users-list.component';
 import { CommonModule } from '@angular/common';
-import { GeneralInformationsComponent } from './general-informations/general-informations.component';
+import { UsersListComponent } from './users-list/users-list.component';
+import { AngularMaterialModule } from '../angular-material/angular-material.module';
 import { UserInfoItemComponent } from './user-info-item/user-info-item.component';
+import { GeneralInformationsComponent } from './general-informations/general-informations.component';
+import { ContactInformationsComponent } from './contact-informations/contact-informations.component';
 
 @NgModule({
     declarations: [
         UsersListComponent,
         UserInfoItemComponent,
         GeneralInformationsComponent,
+        ContactInformationsComponent,
     ],
     imports: [
         PipesModule,
@@ -19,7 +21,8 @@ import { UserInfoItemComponent } from './user-info-item/user-info-item.component
     ],
     exports: [
         UsersListComponent,
-        GeneralInformationsComponent
+        GeneralInformationsComponent,
+        ContactInformationsComponent
     ],
 })
 export class ComponentsModule { }
