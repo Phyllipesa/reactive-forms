6426bb325365cbc6436edd2f330dc014de2648ff
Phyllipe <phyllipe.sjc@hotmail.com>
2024-12-13 17:06:28 -0300

    Adicionando lógica do display dos endereços ao AddressListComponent

--- src/app/components/contact-informations/components/address-list/address-list.component.ts
+++ src/app/components/contact-informations/components/address-list/address-list.component.ts
@@ -1,11 +1,57 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
+
+import { IAddress } from '../../../../interfaces/user/address';
 import { AddressList } from '../../../../type/address-list';
+import { AddressTypeEnum } from '../../../../enums/address-type.enum';
+import { IAddressToDisplay } from '../../../../interfaces/address-to-display';
+import { addressTypeDescriptionMap } from '../../../../utils/address-type-description-map';
 
 @Component({
   selector: 'app-address-list',
   templateUrl: './address-list.component.html',
   styleUrl: './address-list.component.scss'
 })
-export class AddressListComponent {
+export class AddressListComponent implements OnChanges {
+  addressListToDisplay: IAddressToDisplay[] = [];
+
   @Input({ required: true }) userAddressList: AddressList | undefined = [];
+
+
+  ngOnChanges(changes: SimpleChanges) {
+    const ADDRESS_LIST_LOADED = Array.isArray(changes['userAddressList'].currentValue)
+
+    if (ADDRESS_LIST_LOADED) {
+      this.prepareAddressListToDisplay();
+    }
+  }
+
+  private prepareAddressListToDisplay() {
+    this.addressListToDisplay = [];
+
+    Object.keys(addressTypeDescriptionMap).map(Number).forEach((addressType: number) => {
+      const addressFound = this.userAddressList?.find((userAddress) => userAddress.type === addressType);
+
+      this.addressListToDisplay.push(this.returnAddressToDisplay(addressFound, addressType));
+    });
+  }
+
+  returnAddressToDisplay(address: IAddress | undefined, addressType: number): IAddressToDisplay {
+    if (!address) {
+      return {
+        typeDescription: addressTypeDescriptionMap[addressType as AddressTypeEnum],
+        type: addressType,
+        street: '-',
+        complement: '-',
+        country: '-',
+        state: '-',
+        city: '-',
+      }
+    }
+
+    return {
+      typeDescription: addressTypeDescriptionMap[addressType as AddressTypeEnum],
+      ...address,
+    }
+  }
 }
+
