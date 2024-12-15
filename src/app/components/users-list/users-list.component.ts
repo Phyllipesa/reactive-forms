bf67f18f57d3f52f1bed32b14584bdcd8007f03a
Phyllipe <phyllipe.sjc@hotmail.com>
2024-12-15 13:22:21 -0300

    Implementação da lógica de seleção de usuário

--- src/app/components/users-list/users-list.component.ts
+++ src/app/components/users-list/users-list.component.ts
@@ -1,12 +1,19 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { UserListResponse } from '../../type/user-list-response';
 
 @Component({
-  selector: 'app-users-list',
-  templateUrl: './users-list.component.html',
-  styleUrl: './users-list.component.scss'
+	selector: 'app-users-list',
+	templateUrl: './users-list.component.html',
+	styleUrl: './users-list.component.scss'
 })
 export class UsersListComponent {
-  @Input({ required: true }) usersList: UserListResponse = [];
+	userSelectedIndex: number | undefined;
 
+	@Input({ required: true }) usersList: UserListResponse = [];
+	@Output('onUserSelected') onUserSelectedEmitt = new EventEmitter<number>();
+
+	onUserSelected(userIndex: number) {
+		this.userSelectedIndex = userIndex;
+		this.onUserSelectedEmitt.emit(userIndex);
+	}
 }
