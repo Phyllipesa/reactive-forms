8f5f432ac35be565028ae2893f3bd0ac9ea1b05e
Phyllipe <phyllipe.sjc@hotmail.com>
2024-12-15 17:26:46 -0300

    Criando UserInformationsContainerComponent

---
+++ src/app/components/user-informations-container/user-informations-container.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user-informations-container',
+  templateUrl: './user-informations-container.component.html',
+  styleUrl: './user-informations-container.component.scss'
+})
+export class UserInformationsContainerComponent {
+
+}
