f61087b13adfc32b38ececec4d1131437127ddd3
Phyllipe <phyllipe.sjc@hotmail.com>
2024-12-15 09:58:43 -0300

    Adicionando Input dependentsList

--- src/app/components/dependence-list/dependence-list.component.ts
+++ src/app/components/dependence-list/dependence-list.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { DependentsList } from '../../type/dependents-list';
 
 @Component({
-  selector: 'app-dependence-list',
-  templateUrl: './dependence-list.component.html',
-  styleUrl: './dependence-list.component.scss'
+	selector: 'app-dependence-list',
+	templateUrl: './dependence-list.component.html',
+	styleUrl: './dependence-list.component.scss'
 })
 export class DependenceListComponent {
-
+	@Input({ required: true }) dependentsList: DependentsList | undefined = [];
 }
