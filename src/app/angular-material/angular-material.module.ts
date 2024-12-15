3cffd99f4b63dbc5593de91f7e44991a7e6e8870
Phyllipe <phyllipe.sjc@hotmail.com>
2024-12-15 18:52:14 -0300

    Adicionando import MatInputModule e MatFormFieldModule

--- src/app/angular-material/angular-material.module.ts
+++ src/app/angular-material/angular-material.module.ts
@@ -1,15 +1,21 @@
 import { NgModule } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import { MatTabsModule } from '@angular/material/tabs';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 @NgModule({
     imports: [
         MatCardModule,
-        MatTabsModule
+        MatTabsModule,
+        MatInputModule,
+        MatFormFieldModule,
     ],
     exports: [
         MatCardModule,
-        MatTabsModule
+        MatTabsModule,
+        MatInputModule,
+        MatFormFieldModule,
     ],
 })
 export class AngularMaterialModule { }
