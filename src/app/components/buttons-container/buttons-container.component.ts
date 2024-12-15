317f38d08381715b73ba8c62d940d479e7398d00
Phyllipe <phyllipe.sjc@hotmail.com>
2024-12-15 16:12:03 -0300

    Criando o ButtonsContainerComponent

---
+++ src/app/components/buttons-container/buttons-container.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-buttons-container',
+  templateUrl: './buttons-container.component.html',
+  styleUrl: './buttons-container.component.scss'
+})
+export class ButtonsContainerComponent {
+
+}
