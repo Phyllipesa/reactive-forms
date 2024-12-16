0c2297314cc806b2c7a52244b57796f534e332a5
Phyllipe <phyllipe.sjc@hotmail.com>
2024-12-16 16:57:47 -0300

    Criado AddressListEditComponent

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -6,14 +6,15 @@ import { PhoneListComponent } from './contact-informations/components/phone-list
 import { AddressListComponent } from './contact-informations/components/address-list/address-list.component';
 import { AngularMaterialModule } from '../angular-material/angular-material.module';
 import { UserInfoItemComponent } from './user-info-item/user-info-item.component';
+import { PhoneListEditComponent } from './contact-informations-edit/components/phone-list-edit/phone-list-edit.component';
 import { DependenceListComponent } from './dependence-list/dependence-list.component';
+import { AddressListEditComponent } from './contact-informations-edit/components/address-list-edit/address-list-edit.component';
 import { ButtonsContainerComponent } from './buttons-container/buttons-container.component';
 import { GeneralInformationsComponent } from './general-informations/general-informations.component';
 import { ContactInformationsComponent } from './contact-informations/contact-informations.component';
 import { GeneralInformationsEditComponent } from './general-informations-edit/general-informations-edit.component';
 import { ContactInformationsEditComponent } from './contact-informations-edit/contact-informations-edit.component';
 import { UserInformationsContainerComponent } from './user-informations-container/user-informations-container.component';
-import { PhoneListEditComponent } from './contact-informations-edit/components/phone-list-edit/phone-list-edit.component';
 
 @NgModule({
     declarations: [
@@ -21,14 +22,15 @@ import { PhoneListEditComponent } from './contact-informations-edit/components/p
         PhoneListComponent,
         AddressListComponent,
         UserInfoItemComponent,
+        PhoneListEditComponent,
         DependenceListComponent,
+        AddressListEditComponent,
         ButtonsContainerComponent,
         GeneralInformationsComponent,
         ContactInformationsComponent,
         GeneralInformationsEditComponent,
         ContactInformationsEditComponent,
         UserInformationsContainerComponent,
-        PhoneListEditComponent,
     ],
     imports: [
         PipesModule,
