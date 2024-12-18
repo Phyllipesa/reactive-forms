20da62f132c4f9e8762e7123cac784fa5571d99b
Phyllipe <phyllipe.sjc@hotmail.com>
2024-12-18 14:46:35 -0300

    Passando o userSelected para o UserFormController

--- src/app/components/user-informations-container/user-informations-container.component.ts
+++ src/app/components/user-informations-container/user-informations-container.component.ts
@@ -1,19 +1,26 @@
 import { IUser } from '../../interfaces/user/user';
 
 import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
+import { UserFormController } from './user-form-controller';
 
 @Component({
   selector: 'app-user-informations-container',
   templateUrl: './user-informations-container.component.html',
   styleUrl: './user-informations-container.component.scss'
 })
-export class UserInformationsContainerComponent implements OnChanges {
+export class UserInformationsContainerComponent extends UserFormController implements OnChanges {
   currentTabIndex: number = 0;  // Define qual aba do TabsGroup será mostrada ao carregar a pagina.
 
   @Input({ required: true }) isInEditMode: boolean = false;
   @Input({ required: true }) userSelected: IUser = {} as IUser;
 
-  ngOnChanges(_: SimpleChanges) {
+  ngOnChanges(changes: SimpleChanges) {
     this.currentTabIndex = 0;
+
+    const HAS_USER_SELECTED = changes['userSelected'] && Object.keys(changes['userSelected'].currentValue).length > 0;
+
+    if (HAS_USER_SELECTED) {
+      this.fulfillUserForm(this.userSelected);
+    }
   }
 }
