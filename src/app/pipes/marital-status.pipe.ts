6660c5e1a8249d8173319dff5d0f1bc6f1d9fcf2
Phyllipe <phyllipe.sjc@hotmail.com>
2024-12-19 17:11:41 -0300

    Refatoranção da MaritalStatusPipe

--- src/app/pipes/marital-status.pipe.ts
+++ src/app/pipes/marital-status.pipe.ts
@@ -1,19 +1,13 @@
 import { Pipe, PipeTransform } from '@angular/core';
 import { MaritalStatusEnum } from '../enums/marital-status.enum';
+import { maritalStatusDescriptionMap } from '../utils/marital-type-description-map';
 
 @Pipe({
 	name: 'maritalStatus'
 })
 export class MaritalStatusPipe implements PipeTransform {
 	transform(matiralStatus: number | undefined): string {
-		const maritalStatusMap: { [key in MaritalStatusEnum]: string } = {
-			[MaritalStatusEnum.SINGLE]: 'Solteiro',
-			[MaritalStatusEnum.MARRIED]: 'Casado',
-			[MaritalStatusEnum.DIVORCED]: 'Divorciado',
-		};
-
-		return matiralStatus ? maritalStatusMap[matiralStatus as MaritalStatusEnum] : '';
+		return matiralStatus ? maritalStatusDescriptionMap[matiralStatus as MaritalStatusEnum] : '';
 	}
-
 }
 
