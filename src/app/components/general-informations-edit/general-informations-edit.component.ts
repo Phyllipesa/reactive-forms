a01e769f6d1c8791e427fd9444ed5108fa3cac7e
Phyllipe <phyllipe.sjc@hotmail.com>
2024-12-19 14:33:10 -0300

    Adicionando a lógica de filtro para o countriesListFiltered

--- src/app/components/general-informations-edit/general-informations-edit.component.ts
+++ src/app/components/general-informations-edit/general-informations-edit.component.ts
@@ -1,23 +1,56 @@
 import { FormControl, FormGroup } from '@angular/forms';
-import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
+import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
 
 import { CountriesList } from '../../type/countries-list';
+import { MatAutocompleteSelectedEvent } from '@angular/material/autocomplete';
 
 @Component({
-  selector: 'app-general-informations-edit',
-  templateUrl: './general-informations-edit.component.html',
-  styleUrl: './general-informations-edit.component.scss'
+    selector: 'app-general-informations-edit',
+    templateUrl: './general-informations-edit.component.html',
+    styleUrl: './general-informations-edit.component.scss'
 })
-export class GeneralInformationsEditComponent implements OnChanges {
-  @Input({ required: true }) userForm!: FormGroup;
-  @Input({ required: true }) countriesList: CountriesList = [];
+export class GeneralInformationsEditComponent implements OnInit, OnChanges {
+    @Input({ required: true }) userForm!: FormGroup;
+    @Input({ required: true }) countriesList: CountriesList = [];
+    countriesListFiltered: CountriesList = [];
 
-  ngOnChanges(changes: SimpleChanges) {
-    console.log(changes);
-    
-  }
+    ngOnInit() {
+        this.watchCountryFormChangeAndFilter();
+        console.log(this.userForm);
+    }
 
-  get emailControl(): FormControl {
-    return this.userForm.get('generalInformations.email') as FormControl;
-  }
+    ngOnChanges(changes: SimpleChanges) {
+        this.countriesListFiltered = this.countriesList;
+    }
+
+    get emailControl(): FormControl {
+        return this.userForm.get('generalInformations.email') as FormControl;
+    }
+
+    get countryControl(): FormControl {
+        return this.userForm.get('generalInformations.country') as FormControl;
+    }
+
+    onCountrySelected(event: MatAutocompleteSelectedEvent) {
+        console.log(event.option.value);
+    }
+
+    /**
+     * Passando "this.filterCountriesList" o subscribe não sabe o contexto do "this.filterCountriesList"
+     * que estamos passando. Na função interna do subscribe o contexto é diferente, ele não sabe ao que o "this"
+     * se referência, não sabe que o "this" esta fazendo referência a nossa classe GeneralInformationsEditComponent.
+     * 
+     * Para resolver isso, podemos usar o bind para passar o contexto da classe 
+     *  - this.filterCountriesList.bind(this)
+     */
+    private watchCountryFormChangeAndFilter() {
+        this.countryControl.valueChanges.subscribe(this.filterCountriesList.bind(this)); // 
+        // this.countryControl.valueChanges.subscribe((value: string ) => this.filterCountriesList(value));
+    }
+
+    private filterCountriesList(searchTerm: string) {
+        this.countriesListFiltered = this.countriesList.filter(
+            (country) => country.name.toLowerCase().includes(searchTerm.toLowerCase().trim())
+        );
+    }
 }
