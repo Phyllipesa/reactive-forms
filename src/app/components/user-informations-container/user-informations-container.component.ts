5e994c33b0004939ed42dbbd4da9133d3c25cb05
Phyllipe <phyllipe.sjc@hotmail.com>
2024-12-19 16:45:38 -0300

    Configurando o getStatesList e a lógica para atualização da statesList

--- src/app/components/user-informations-container/user-informations-container.component.ts
+++ src/app/components/user-informations-container/user-informations-container.component.ts
@@ -3,7 +3,9 @@ import { take } from 'rxjs';
 import { Component, inject, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
 
 import { IUser } from '../../interfaces/user/user';
+import { StatesList } from '../../type/states-list';
 import { CountriesList } from '../../type/countries-list';
+import { StatesService } from '../../services/states.service';
 import { CountriesService } from '../../services/countries.service';
 import { UserFormController } from './user-form-controller';
 
@@ -14,8 +16,10 @@ import { UserFormController } from './user-form-controller';
 })
 export class UserInformationsContainerComponent extends UserFormController implements OnInit, OnChanges {
     currentTabIndex: number = 0;  // Define qual aba do TabsGroup será mostrada ao carregar a pagina.
+    statesList: StatesList = [];
     countriesList: CountriesList = [];
 
+    private readonly _statesService = inject(StatesService);
     private readonly _countriesService = inject(CountriesService);
 
     @Input({ required: true }) isInEditMode: boolean = false;
@@ -24,19 +28,30 @@ export class UserInformationsContainerComponent extends UserFormController imple
     ngOnInit() {
         this.getCountriesList();
     }
-    
+
     ngOnChanges(changes: SimpleChanges) {
         this.currentTabIndex = 0;
-        
+
         const HAS_USER_SELECTED = changes['userSelected'] && Object.keys(changes['userSelected'].currentValue).length > 0;
-        
+
         if (HAS_USER_SELECTED) {
             this.fulfillUserForm(this.userSelected);
+            this.getStatesList(this.userSelected.country);
         }
     }
 
+    onCountrySelected(contryName: string) {
+        this.getStatesList(contryName);
+    }
+
+    private getStatesList(country: string) {
+        this._statesService.getStates(country).pipe(take(1)).subscribe((statesList: StatesList) => {
+            this.statesList = statesList;
+        });
+    }
+
     private getCountriesList() {
-        this._countriesService.getCountries().pipe(take(1)).subscribe((countriesList: CountriesList)=> {
+        this._countriesService.getCountries().pipe(take(1)).subscribe((countriesList: CountriesList) => {
             this.countriesList = countriesList
         });
     }
