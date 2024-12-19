a4e532161636f21ea221645300a9463e7179f63e
Phyllipe <phyllipe.sjc@hotmail.com>
2024-12-19 13:50:24 -0300

    Configurando o getCountriesList

--- src/app/components/user-informations-container/user-informations-container.component.ts
+++ src/app/components/user-informations-container/user-informations-container.component.ts
@@ -1,26 +1,43 @@
-import { IUser } from '../../interfaces/user/user';
+import { take } from 'rxjs';
 
-import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
+import { Component, inject, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
+
+import { IUser } from '../../interfaces/user/user';
+import { CountriesList } from '../../type/countries-list';
+import { CountriesService } from '../../services/countries.service';
 import { UserFormController } from './user-form-controller';
 
 @Component({
-  selector: 'app-user-informations-container',
-  templateUrl: './user-informations-container.component.html',
-  styleUrl: './user-informations-container.component.scss'
+    selector: 'app-user-informations-container',
+    templateUrl: './user-informations-container.component.html',
+    styleUrl: './user-informations-container.component.scss'
 })
-export class UserInformationsContainerComponent extends UserFormController implements OnChanges {
-  currentTabIndex: number = 0;  // Define qual aba do TabsGroup será mostrada ao carregar a pagina.
+export class UserInformationsContainerComponent extends UserFormController implements OnInit, OnChanges {
+    currentTabIndex: number = 0;  // Define qual aba do TabsGroup será mostrada ao carregar a pagina.
+    countriesList: CountriesList = [];
 
-  @Input({ required: true }) isInEditMode: boolean = false;
-  @Input({ required: true }) userSelected: IUser = {} as IUser;
+    private readonly _countriesService = inject(CountriesService);
 
-  ngOnChanges(changes: SimpleChanges) {
-    this.currentTabIndex = 0;
+    @Input({ required: true }) isInEditMode: boolean = false;
+    @Input({ required: true }) userSelected: IUser = {} as IUser;
 
-    const HAS_USER_SELECTED = changes['userSelected'] && Object.keys(changes['userSelected'].currentValue).length > 0;
-
-    if (HAS_USER_SELECTED) {
-      this.fulfillUserForm(this.userSelected);
+    ngOnInit() {
+        this.getCountriesList();
     }
-  }
+    
+    ngOnChanges(changes: SimpleChanges) {
+        this.currentTabIndex = 0;
+        
+        const HAS_USER_SELECTED = changes['userSelected'] && Object.keys(changes['userSelected'].currentValue).length > 0;
+        
+        if (HAS_USER_SELECTED) {
+            this.fulfillUserForm(this.userSelected);
+        }
+    }
+
+    private getCountriesList() {
+        this._countriesService.getCountries().pipe(take(1)).subscribe((countriesList: CountriesList)=> {
+            this.countriesList = countriesList
+        });
+    }
 }
