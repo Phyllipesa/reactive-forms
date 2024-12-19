129cba824559595fa605842a11aeb7a003e622aa
Phyllipe <phyllipe.sjc@hotmail.com>
2024-12-19 13:51:07 -0300

    Recebendo o countriesList do UserInformationsContainerComponent

--- src/app/components/general-informations-edit/general-informations-edit.component.ts
+++ src/app/components/general-informations-edit/general-informations-edit.component.ts
@@ -1,17 +1,23 @@
-import { Component, Input } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
+import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
+
+import { CountriesList } from '../../type/countries-list';
 
 @Component({
   selector: 'app-general-informations-edit',
   templateUrl: './general-informations-edit.component.html',
   styleUrl: './general-informations-edit.component.scss'
 })
-export class GeneralInformationsEditComponent {
+export class GeneralInformationsEditComponent implements OnChanges {
   @Input({ required: true }) userForm!: FormGroup;
+  @Input({ required: true }) countriesList: CountriesList = [];
+
+  ngOnChanges(changes: SimpleChanges) {
+    console.log(changes);
+    
+  }
 
   get emailControl(): FormControl {
     return this.userForm.get('generalInformations.email') as FormControl;
   }
-
-  
 }
