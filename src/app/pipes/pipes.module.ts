def91ff76fb94cc68bf1ab62f7041acb671066a1
Phyllipe <phyllipe.sjc@hotmail.com>
2024-12-20 16:52:22 -0300

    Exportando o PhoneMaskPipe

--- src/app/pipes/pipes.module.ts
+++ src/app/pipes/pipes.module.ts
@@ -1,14 +1,17 @@
 import { CpfPipe } from './cpf.pipe';
 import { NgModule } from '@angular/core';
 import { MaritalStatusPipe } from './marital-status.pipe';
+import { PhoneMaskPipe } from './phone-mask.pipe';
 
 @NgModule({
   declarations: [
     CpfPipe,
+    PhoneMaskPipe,
     MaritalStatusPipe,
   ],
   exports: [
     CpfPipe,
+    PhoneMaskPipe,
     MaritalStatusPipe,
   ],
 })
