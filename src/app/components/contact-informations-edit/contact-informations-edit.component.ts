6714fffe160b4a26d79c58ff6870cde756e47160
Phyllipe <phyllipe.sjc@hotmail.com>
2024-12-20 16:17:34 -0300

    Criando input userForm no ContactInformationsEditComponent

--- src/app/components/contact-informations-edit/contact-informations-edit.component.ts
+++ src/app/components/contact-informations-edit/contact-informations-edit.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
+import { FormGroup } from '@angular/forms';
+
+import { Component, Input } from '@angular/core';
 
 @Component({
-  selector: 'app-contact-informations-edit',
-  templateUrl: './contact-informations-edit.component.html',
-  styleUrl: './contact-informations-edit.component.scss'
+    selector: 'app-contact-informations-edit',
+    templateUrl: './contact-informations-edit.component.html',
+    styleUrl: './contact-informations-edit.component.scss'
 })
 export class ContactInformationsEditComponent {
-
+    @Input({ required: true }) userForm!: FormGroup;
 }
