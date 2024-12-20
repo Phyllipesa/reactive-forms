fc2a84f044c6b772c4a90f0e85a5abf24d5faaa4
Phyllipe <phyllipe.sjc@hotmail.com>
2024-12-20 16:18:00 -0300

    Criando input userForm no PhoneListEditComponent

--- src/app/components/contact-informations-edit/components/phone-list-edit/phone-list-edit.component.ts
+++ src/app/components/contact-informations-edit/components/phone-list-edit/phone-list-edit.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
+import { FormGroup } from '@angular/forms';
+
+import { Component, Input } from '@angular/core';
 
 @Component({
-  selector: 'app-phone-list-edit',
-  templateUrl: './phone-list-edit.component.html',
-  styleUrl: './phone-list-edit.component.scss'
+    selector: 'app-phone-list-edit',
+    templateUrl: './phone-list-edit.component.html',
+    styleUrl: './phone-list-edit.component.scss'
 })
 export class PhoneListEditComponent {
-
+    @Input({ required: true }) userForm!: FormGroup;
 }
