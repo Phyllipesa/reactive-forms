fae1506a9338693ebefa0e6a45be77a26db6e601
Phyllipe <phyllipe.sjc@hotmail.com>
2024-12-20 17:02:15 -0300

    Criando a Lógica do PhoneMaskPipe

--- src/app/pipes/phone-mask.pipe.ts
+++ src/app/pipes/phone-mask.pipe.ts
@@ -1,12 +1,18 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
+import { PhoneTypeEnum } from '../enums/phone-type.enum';
+
 @Pipe({
-  name: 'phoneMask'
+    name: 'phoneMask'
 })
 export class PhoneMaskPipe implements PipeTransform {
+    transform(phoneType: number): string {
+        const phoneMaskMap: { [key in PhoneTypeEnum]: string } = {
+            [PhoneTypeEnum.RESIDENTIAL]: '+00 00 0000-0000',
+            [PhoneTypeEnum.MOBILE]: '+00 00 00000-0000',
+            [PhoneTypeEnum.EMERGENCY]: '+00 00 0000-0000||+00 00 00000-0000',
+        };
 
-  transform(value: unknown, ...args: unknown[]): unknown {
-    return null;
-  }
-
+        return phoneMaskMap[phoneType as PhoneTypeEnum];
+    };
 }
