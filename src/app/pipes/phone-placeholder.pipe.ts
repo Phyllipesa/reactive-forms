ebefdf9a7b25cc5fe8aeb531a94731ea25d331af
Phyllipe <phyllipe.sjc@hotmail.com>
2024-12-20 17:14:19 -0300

    Criando a lógica do PhonePlaceholderPipe

--- src/app/pipes/phone-placeholder.pipe.ts
+++ src/app/pipes/phone-placeholder.pipe.ts
@@ -1,12 +1,17 @@
 import { Pipe, PipeTransform } from '@angular/core';
+import { PhoneTypeEnum } from '../enums/phone-type.enum';
 
 @Pipe({
   name: 'phonePlaceholder'
 })
 export class PhonePlaceholderPipe implements PipeTransform {
+  transform(phoneType: number): string {
+        const phoneMaskMap: { [key in PhoneTypeEnum]: string } = {
+            [PhoneTypeEnum.RESIDENTIAL]: '+55 11 1234-1234',
+            [PhoneTypeEnum.MOBILE]: '+55 11 91234-1234',
+            [PhoneTypeEnum.EMERGENCY]: '+55 11 1234-1234 ou +55 11 91234-1234',
+        };
 
-  transform(value: unknown, ...args: unknown[]): unknown {
-    return null;
-  }
-
+        return phoneMaskMap[phoneType as PhoneTypeEnum];
+    };
 }
