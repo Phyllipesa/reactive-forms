3ae3f338251ec22fc6ebc800cb188fe70337d218
Phyllipe <phyllipe.sjc@hotmail.com>
2024-12-20 17:04:37 -0300

    Exportando PhonePlaceholderPipe

--- src/app/pipes/pipes.module.ts
+++ src/app/pipes/pipes.module.ts
@@ -1,18 +1,22 @@
 import { CpfPipe } from './cpf.pipe';
 import { NgModule } from '@angular/core';
-import { MaritalStatusPipe } from './marital-status.pipe';
+
 import { PhoneMaskPipe } from './phone-mask.pipe';
+import { MaritalStatusPipe } from './marital-status.pipe';
+import { PhonePlaceholderPipe } from './phone-placeholder.pipe';
 
 @NgModule({
   declarations: [
     CpfPipe,
     PhoneMaskPipe,
     MaritalStatusPipe,
+    PhonePlaceholderPipe,
   ],
   exports: [
     CpfPipe,
     PhoneMaskPipe,
     MaritalStatusPipe,
+    PhonePlaceholderPipe,
   ],
 })
 export class PipesModule { }
