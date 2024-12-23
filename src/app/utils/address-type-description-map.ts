006f604ecb0b34a628bcae4c0ffaedcffa319c23
Phyllipe <phyllipe.sjc@hotmail.com>
2024-12-23 14:45:30 -0300

    Correção: Letras maiúsculas em todos os values

--- src/app/utils/address-type-description-map.ts
+++ src/app/utils/address-type-description-map.ts
@@ -2,6 +2,6 @@ import { AddressTypeEnum } from "../enums/address-type.enum";
 
 export const addressTypeDescriptionMap: { [key in AddressTypeEnum]: string } = {
     [AddressTypeEnum.RESIDENTIAL]: 'Residencial',
-    [AddressTypeEnum.WORK]: 'trabalho',
-    [AddressTypeEnum.ALTERNATIVE]: 'alternativo',
+    [AddressTypeEnum.WORK]: 'Trabalho',
+    [AddressTypeEnum.ALTERNATIVE]: 'Alternativo',
 };
