c3d8a69f245f04fbc86a36d601bdfc1a730f5532
Phyllipe <phyllipe.sjc@hotmail.com>
2024-12-23 13:22:16 -0300

    Refatorando AddressListComponent para funcionar tanto para o modo display e edit

--- src/app/components/contact-informations/components/address-list/address-list.component.ts
+++ src/app/components/contact-informations/components/address-list/address-list.component.ts
@@ -1,57 +1,35 @@
 import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
 
-import { IAddress } from '../../../../interfaces/user/address';
 import { AddressList } from '../../../../type/address-list';
-import { AddressTypeEnum } from '../../../../enums/address-type.enum';
 import { IAddressToDisplay } from '../../../../interfaces/address-to-display';
-import { addressTypeDescriptionMap } from '../../../../utils/address-type-description-map';
+import { prepareAddressList } from '../../../../utils/prepared-address-list';
 
 @Component({
-  selector: 'app-address-list',
-  templateUrl: './address-list.component.html',
-  styleUrl: './address-list.component.scss'
+    selector: 'app-address-list',
+    templateUrl: './address-list.component.html',
+    styleUrl: './address-list.component.scss'
 })
 export class AddressListComponent implements OnChanges {
-  addressListToDisplay: IAddressToDisplay[] = [];
+    addressListToDisplay: IAddressToDisplay[] = [];
 
-  @Input({ required: true }) userAddressList: AddressList | undefined = [];
+    @Input({ required: true }) userAddressList: AddressList | undefined = [];
 
+    ngOnChanges(changes: SimpleChanges) {
+        const ADDRESS_LIST_LOADED = Array.isArray(changes['userAddressList'].currentValue);
 
-  ngOnChanges(changes: SimpleChanges) {
-    const ADDRESS_LIST_LOADED = Array.isArray(changes['userAddressList'].currentValue)
+        if (ADDRESS_LIST_LOADED) {
+            this.prepareAddressListToDisplay();
+        };
+    };
 
-    if (ADDRESS_LIST_LOADED) {
-      this.prepareAddressListToDisplay();
-    }
-  }
+    private prepareAddressListToDisplay() {
+        this.addressListToDisplay = [];
 
-  private prepareAddressListToDisplay() {
-    this.addressListToDisplay = [];
+        const originalAddressList = this.userAddressList && this.userAddressList.length > 0 ? this.userAddressList : [];
 
-    Object.keys(addressTypeDescriptionMap).map(Number).forEach((addressType: number) => {
-      const addressFound = this.userAddressList?.find((userAddress) => userAddress.type === addressType);
-
-      this.addressListToDisplay.push(this.returnAddressToDisplay(addressFound, addressType));
-    });
-  }
-
-  returnAddressToDisplay(address: IAddress | undefined, addressType: number): IAddressToDisplay {
-    if (!address) {
-      return {
-        typeDescription: addressTypeDescriptionMap[addressType as AddressTypeEnum],
-        type: addressType,
-        street: '-',
-        complement: '-',
-        country: '-',
-        state: '-',
-        city: '-',
-      }
-    }
-
-    return {
-      typeDescription: addressTypeDescriptionMap[addressType as AddressTypeEnum],
-      ...address,
-    }
-  }
-}
+        prepareAddressList(originalAddressList, true, (address) => {
+            this.addressListToDisplay.push(address);
+        });
+    };
+};
 
