b9ce66487ae8719310b307a648609b8e46e838c7
Phyllipe <phyllipe.sjc@hotmail.com>
2024-12-23 16:45:45 -0300

    Criando a input userForm no DependentsListEditComponent e um alias para o dependentsList

--- src/app/components/dependents-list-edit/dependents-list-edit.component.ts
+++ src/app/components/dependents-list-edit/dependents-list-edit.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { FormArray, FormGroup } from '@angular/forms';
 
 @Component({
-  selector: 'app-dependents-list-edit',
-  templateUrl: './dependents-list-edit.component.html',
-  styleUrl: './dependents-list-edit.component.scss'
+    selector: 'app-dependents-list-edit',
+    templateUrl: './dependents-list-edit.component.html',
+    styleUrl: './dependents-list-edit.component.scss'
 })
 export class DependentsListEditComponent {
+    @Input({ required: true }) userForm!: FormGroup;
 
+    get dependentsList(): FormArray {
+        return this.userForm.get('dependentsList') as FormArray;
+    };
 }
