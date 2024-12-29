23341b85cb574868ed74e2a3a91e676cdd1bcd77
Phyllipe <phyllipe.sjc@hotmail.com>
2024-12-29 15:04:32 -0300

    Criando as interfaces para o userFormRaw

---
+++ src/app/interfaces/user-form/user-form-contact-informations.ts
@@ -0,0 +1,7 @@
+import { IUserFormPhone } from "./user-form-phone"
+import { IUserFormAddress } from "./user-form-address"
+
+export interface IUserFormContactInformations {
+    phoneList: IUserFormPhone[]
+    addressList: IUserFormAddress[]
+};
---
+++ src/app/interfaces/user-form/user-form.ts
@@ -0,0 +1,9 @@
+import { IUserFormDependent } from "./user-form-dependent"
+import { IUserFormContactInformations } from "./user-form-contact-informations"
+import { IUserFormGeneralInformations } from "./user-form-general-informations"
+
+export interface IUserForm {
+    generalInformations: IUserFormGeneralInformations
+    contactInformations: IUserFormContactInformations
+    dependentsList: IUserFormDependent[]
+};
