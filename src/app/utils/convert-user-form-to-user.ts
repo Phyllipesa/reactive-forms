d7e87363f829e9a1b85ebe1220cb285179a733ed
Phyllipe <phyllipe.sjc@hotmail.com>
2024-12-29 16:20:29 -0300

    Criado função convertPhoneList

--- src/app/utils/convert-user-form-to-user.ts
+++ src/app/utils/convert-user-form-to-user.ts
@@ -1,14 +1,15 @@
 import { IUser } from "../interfaces/user/user";
 import { IUserForm } from "../interfaces/user-form/user-form";
+import { PhoneList } from "../type/phone-list";
+import { IUserFormPhone } from "../interfaces/user-form/user-form-phone";
 import { convertDateObjToPtBrDate } from "./convert-date-obj-to-pt-br-date";
 import { IUserFormGeneralInformations } from "../interfaces/user-form/user-form-general-informations";
 
 export const convertUserFormToUser = (userForm: IUserForm): IUser => {
     let newUser: Partial<IUser> = {} as IUser;
 
-    newUser = {
-        ...convertGeneralInformations(userForm.generalInformations),
-    };
+    newUser = { ...convertGeneralInformations(userForm.generalInformations) };
+    newUser.phoneList = convertPhoneList(userForm.contactInformations.phoneList);
 
     return newUser as IUser;
 };
@@ -25,4 +26,15 @@ const convertGeneralInformations = (
         monthlyIncome: generalInformations.monthlyIncome,
         birthDate: convertDateObjToPtBrDate(generalInformations.birthDate),
     };
+};
+
+const convertPhoneList = (phoneList: IUserFormPhone[]): PhoneList => {
+    const newUserPhoneList: PhoneList = phoneList.map((phone: IUserFormPhone) => ({
+        type: phone.type,
+        internationalCode: phone.number.substring(0, 2),
+        areaCode: phone.number.substring(2, 4),
+        number: phone.number.substring(4),
+    }));
+
+    return newUserPhoneList;
 };
